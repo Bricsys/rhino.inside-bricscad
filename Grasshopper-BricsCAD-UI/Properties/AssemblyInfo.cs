b77f53852f80cdc9edbc413d5ab56b83db55742b
Bogdan Grebenshchikov <49469073+BgdnG@users.noreply.github.com>
2020-04-27 16:43:28 +0700

    Updated Grasshopper-BricsCAD-UI proj with new GrasshopperData tools

--- Grasshopper-BricsCAD-UI/Properties/AssemblyInfo.cs
+++ Grasshopper-BricsCAD-UI/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bricsys")]
 [assembly: AssemblyProduct("Grasshopper-BricsCAD Connection")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
